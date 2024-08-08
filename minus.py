1aaf39c5d78d41b4eb1456cbb6e17e9057133b70
Geekcrowds <cyclx1@gmail.com>
2024-08-09 00:55:43 +1200

    Create minus.py

---
+++ minus.py
@@ -0,0 +1,19 @@
+# Declare global variable amond number for use in all functions
+amond = str(input('Enter amond number: '))
+
+# Declare global variable taking number for use in all functions
+amond = str(input('Enter taking number: '))
+
+# Define function minus
+def minus(a,b):
+    a -b = c
+    return c
+
+# Define main method that calls other functions
+def main():
+    result = minus(7,5)
+    print(result)
+
+# Execute main() function
+if __name__ == '__main__':
+    main()
